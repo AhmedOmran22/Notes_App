ddb04b6acd0732328865de98246ca9468d3a4e33
AhmedOmran22 <mamdouhanahamada@gmail.com>
2024-06-24 05:18:42 +0300

    colors list view part two

--- lib/widgets/color_item.dart
+++ lib/widgets/color_item.dart
@@ -1,17 +1,34 @@
-
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({
+    super.key,
+    required this.isActive,
+    required this.color,
+  });
+  final bool isActive;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(right: 8),
-      child: CircleAvatar(
-        backgroundColor: Colors.purple,
-        radius: 32,
-      ),
-    );
+    return isActive
+        ? Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 38,
+              child: CircleAvatar(
+                backgroundColor: color,
+                radius: 32,
+              ),
+            ),
+          )
+        : Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 32,
+            ),
+          );
   }
 }
--- lib/widgets/color_item_list_view.dart
+++ lib/widgets/color_item_list_view.dart
@@ -1,18 +1,45 @@
-
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/color_item.dart';
 
-class ColorsListView extends StatelessWidget {
-  const ColorsListView({super.key});
+class ColorsListView extends StatefulWidget {
+  const ColorsListView({
+    super.key,
+    this.isActive,
+  });
 
+  final bool? isActive;
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xffD4C5E2),
+    Color(0xffC9D7F8),
+    Color(0xffA7E2E3),
+    Color(0xff80CFA9),
+    Color(0xff4C6663)
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 64,
+      height: 38 * 2,
       child: ListView.builder(
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (BuildContext context, int index) {
-          return const ColorItem();
+          return GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              color: colors[index],
+              isActive: currentIndex == index,
+            ),
+          );
         },
       ),
     );
